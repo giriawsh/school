c9db55e4571a40119e058eab2b9cc47111155b07
giriawsh <giriawsh@outlook.com>
2020-05-28 00:04:50 +0800

    homework done

--- third/Chapter-3-master/Demo/app/src/main/java/com/example/chapter3/demo/animation/ScalePropertyActivity.java
+++ third/Chapter-3-master/Demo/app/src/main/java/com/example/chapter3/demo/animation/ScalePropertyActivity.java
@@ -19,27 +19,27 @@ public class ScalePropertyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scale_property);
 
-//        View imageView = findViewById(R.id.image_view);
-//        ObjectAnimator scaleXAnimator = ObjectAnimator.ofFloat(imageView,
-//                "scaleX", 1.1f, 0.9f);
-//        scaleXAnimator.setRepeatCount(ValueAnimator.INFINITE);
-//        scaleXAnimator.setInterpolator(new LinearInterpolator());
-//        scaleXAnimator.setDuration(1000);
-//        scaleXAnimator.setRepeatMode(ValueAnimator.REVERSE);
-//
-//        ObjectAnimator scaleYAnimator = ObjectAnimator.ofFloat(imageView,
-//                "scaleY", 1.1f, 0.9f);
-//        scaleYAnimator.setRepeatCount(ValueAnimator.INFINITE);
-//        scaleYAnimator.setInterpolator(new LinearInterpolator());
-//        scaleYAnimator.setDuration(1000);
-//        scaleYAnimator.setRepeatMode(ValueAnimator.REVERSE);
-//
-//        AnimatorSet animatorSet = new AnimatorSet();
-//        animatorSet.playTogether(scaleXAnimator, scaleYAnimator);
-//        animatorSet.start();
+        View imageView = findViewById(R.id.image_view);
+        ObjectAnimator scaleXAnimator = ObjectAnimator.ofFloat(imageView,
+                "scaleX", 1.1f, 0.9f);
+        scaleXAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        scaleXAnimator.setInterpolator(new LinearInterpolator());
+        scaleXAnimator.setDuration(1000);
+        scaleXAnimator.setRepeatMode(ValueAnimator.REVERSE);
 
-        Animator animator = AnimatorInflater.loadAnimator(this, R.animator.breath);
-        animator.setTarget(findViewById(R.id.image_view));
-        animator.start();
+        ObjectAnimator scaleYAnimator = ObjectAnimator.ofFloat(imageView,
+                "scaleY", 1.1f, 0.9f);
+        scaleYAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        scaleYAnimator.setInterpolator(new LinearInterpolator());
+        scaleYAnimator.setDuration(1000);
+        scaleYAnimator.setRepeatMode(ValueAnimator.REVERSE);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(scaleXAnimator, scaleYAnimator);
+        animatorSet.start();
+
+//        Animator animator = AnimatorInflater.loadAnimator(this, R.animator.breath);
+//        animator.setTarget(findViewById(R.id.image_view));
+//        animator.start();
     }
 }
--- third/Chapter-3-master/Homework/app/src/main/java/com/example/chapter3/homework/PlaceholderFragment.java
+++ third/Chapter-3-master/Homework/app/src/main/java/com/example/chapter3/homework/PlaceholderFragment.java
@@ -1,16 +1,36 @@
 package com.example.chapter3.homework;
 
-
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+import java.util.List;
+import java.util.Objects;
 
 public class PlaceholderFragment extends Fragment {
-
-
+    static String[] dataList = {//Beijing University of Posts and Telecommunications
+            "BEIJING",
+            "UNIVERSITY",
+            "OF",
+            "POSTS",
+            "AND",
+            "TELECOMMUNICATIONS",
+            "GIRIA"
+    };
+    ListView listView;
+    LottieAnimationView animationView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -21,12 +41,34 @@ public class PlaceholderFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
+        listView = Objects.requireNonNull(getView()).findViewById(R.id.list_view);
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(Objects.requireNonNull(getActivity()),
+                        android.R.layout.simple_list_item_1, dataList);
+        listView.setAdapter(itemsAdapter);
+        animationView = getView().findViewById(R.id.animation_view3);
         getView().postDelayed(new Runnable() {
             @Override
             public void run() {
                 // 这里会在 5s 后执行
                 // TODO ex3-4：实现动画，将 lottie 控件淡出，列表数据淡入
+                listView.setAlpha(0f);
+                listView.setVisibility(View.VISIBLE);
+
+                listView.animate()
+                        .setDuration(1000)
+                        .alpha(1.0f)
+                        .setListener(null);
+
+                animationView.animate()
+                        .alpha(0f)
+                        .setDuration(1000)
+                        .setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                animationView.setVisibility(View.GONE);
+                            }
+                        });
             }
         }, 5000);
     }
